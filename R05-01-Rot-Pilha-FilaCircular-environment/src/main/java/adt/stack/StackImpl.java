907e0aed2442cc1e2ff57dd03b505c108a315e80
Augusto Santos <augusto.santos@ccc.ufcg.edu.br>
2019-09-17 21:59:36 -0300

    Roteiro 05

---
+++ R05-01-Rot-Pilha-FilaCircular-environment/src/main/java/adt/stack/StackImpl.java
@@ -0,0 +1,54 @@
+package adt.stack;
+
+public class StackImpl<T> implements Stack<T> {
+
+	private T[] array;
+	private int top;
+
+	@SuppressWarnings("unchecked")
+	public StackImpl(int size) {
+		array = (T[]) new Object[size];
+		top = -1;
+	}
+
+	@Override
+	public T top() {
+		T result = null;
+		if (!isFull()) {
+			result = this.array[top];
+		}
+		return result;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return (this.top == -1);
+	}
+
+	@Override
+	public boolean isFull() {
+		return (this.top == array.length - 1);
+	}
+
+	@Override
+	public void push(T element) throws StackOverflowException {
+		if (isFull()) {
+			throw new StackOverflowException();
+		}
+		if (element != null) {
+			this.top++;
+			this.array[top] = element;
+		}
+	}
+
+	@Override
+	public T pop() throws StackUnderflowException {
+		if (isEmpty()) {
+			throw new StackUnderflowException();
+		} else {
+			this.top--;
+			return this.array[top + 1];
+		}
+	}
+
+}
