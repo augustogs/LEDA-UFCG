7687c56df2da50dff5b85579c27348891c313601
Augusto Santos <augusto.santos@ccc.ufcg.edu.br>
2019-10-09 11:27:44 -0300

    Ajuste na implementação.

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,96 +2,77 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
-import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
-		AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size,
-			HashFunctionClosedAddressMethod method) {
-		super(size);
-		hashFunction = new HashFunctionLinearProbing<T>(size, method);
-		this.initiateInternalTable(size);
-	}
+   public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
+      super(size);
+      hashFunction = new HashFunctionLinearProbing<T>(size, method);
+      this.initiateInternalTable(size);
+   }
 
-	@Override
-	public void insert(T element) {
-		if (this.isFull()) {
-			throw new HashtableOverflowException();
-		} else {
-			if (element != null) {
-				int probe = 0;
-				boolean inseriu = false;
-				
-				while (probe < this.capacity() && !inseriu) {
-					int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-					if (this.table[index] == null || this.table[index] instanceof DELETED) {
-						this.table[index] = element;
-						this.elements++;
-						inseriu = true;
-					} else {
-						this.COLLISIONS++;
-					}
-					probe++;
-				}
-			}		
-		}
-	}
+   @Override
+   public void insert(T element) {
+      if (this.isFull()) {
+         throw new HashtableOverflowException();
+      } else {
+         if (element != null) {
+            int probe = 0;
+            boolean inseriu = false;
 
-	@Override
-	public void remove(T element) {
-		int index = indexOf(element);
-		if (index >= 0) {
-			this.table[index] = new DELETED();
-			this.elements--;
-		}
-		
-		
-//		if (element != null && !this.isEmpty()) {
-//			int probe = 0;
-//			boolean removeu = false;
-//			
-//			while (probe < this.capacity() && !removeu) {
-//				int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-//				if (this.table[index] != null && this.table[index].equals(element)) {
-//					this.table[index] = new DELETED();
-//					this.elements--;
-//					removeu = true;
-//				}
-//				probe++;
-//			}
-//		}
-	}
+            while (probe < this.capacity() && !inseriu) {
+               int index = ((HashFunctionLinearProbing<T>) this.hashFunction).hash(element, probe);
+               if (this.table[index] == null || this.table[index] instanceof DELETED) {
+                  this.table[index] = element;
+                  this.elements++;
+                  inseriu = true;
+               } else {
+                  this.COLLISIONS++;
+               }
+               probe++;
+            }
+         }
+      }
+   }
 
-	@Override
-	public T search(T element) {
-		T result = null;
-		
-		int index = indexOf(element);
-		if (index >= 0) {
-			result = (T) this.table[index];
-		}	
-		return result;
-	}
+   @Override
+   public void remove(T element) {
+      int index = indexOf(element);
+      if (index >= 0) {
+         this.table[index] = new DELETED();
+         this.elements--;
+      }
+   }
 
-	@Override
-	public int indexOf(T element) {
-		int result = -1;
-		
-		if (element != null && !this.isEmpty()) {
-			boolean achou = false;
-			int probe = 0;
-			
-			while (probe < this.capacity() && !achou) {
-				int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-				if (this.table[index] != null && this.table[index].equals(element)) {
-					result = index;
-					achou = true;
-				}
-				probe++;
-			}			
-		}	
-		return result;
-	}
+   @Override
+   public T search(T element) {
+      T result = null;
+
+      int index = indexOf(element);
+      if (index >= 0) {
+         result = element;
+      }
+      return result;
+   }
+
+   @Override
+   public int indexOf(T element) {
+      int result = -1;
+
+      if (element != null && !this.isEmpty()) {
+         boolean achou = false;
+         int probe = 0;
+
+         while (probe < this.capacity() && !achou) {
+            int index = ((HashFunctionLinearProbing<T>) this.hashFunction).hash(element, probe);
+            if (this.table[index] != null && this.table[index].equals(element)) {
+               result = index;
+               achou = true;
+            }
+            probe++;
+         }
+      }
+      return result;
+   }
 
 }
--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -1,88 +1,85 @@
 package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
-import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
-public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
-		extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressQuadraticProbingImpl(int size,
-			HashFunctionClosedAddressMethod method, int c1, int c2) {
-		super(size);
-		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
-		this.initiateInternalTable(size);
-	}
+   public HashtableOpenAddressQuadraticProbingImpl(int size, HashFunctionClosedAddressMethod method, int c1, int c2) {
+      super(size);
+      hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
+      this.initiateInternalTable(size);
+   }
 
-	@Override
-	public void insert(T element) {
-		if (this.isFull()) {
-			throw new HashtableOverflowException();
-		} else {
-			if (element != null) {
-				int probe = 0;
-				boolean inseriu = false;
-				
-				while (probe < this.capacity() && !inseriu) {
-					int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-					if (this.table[index] == null || this.table[index] instanceof DELETED) {
-						this.table[index] = element;
-						this.elements++;
-						inseriu = true;
-					} else {
-						this.COLLISIONS++;
-					}
-					probe++;
-				}
-			}		
-		}
-	}
+   @Override
+   public void insert(T element) {
+      if (this.isFull()) {
+         throw new HashtableOverflowException();
+      } else {
+         if (element != null) {
+            int probe = 0;
+            boolean inseriu = false;
 
-	@Override
-	public void remove(T element) {
-		if (element != null && !this.isEmpty()) {
-			int probe = 0;
-			boolean removeu = false;
-			
-			while (probe < this.capacity() && !removeu) {
-				int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-				if (this.table[index] != null && this.table[index].equals(element)) {
-					this.table[index] = new DELETED();
-					this.elements--;
-					removeu = true;
-				}
-				probe++;
-			}
-		}
-	}
+            while (probe < this.capacity() && !inseriu) {
+               int index = ((HashFunctionQuadraticProbing<T>) this.hashFunction).hash(element, probe);
+               if (this.table[index] == null || this.table[index] instanceof DELETED) {
+                  this.table[index] = element;
+                  this.elements++;
+                  inseriu = true;
+               } else {
+                  this.COLLISIONS++;
+               }
+               probe++;
+            }
+         }
+      }
+   }
 
-	@Override
-	public T search(T element) {
-		T result = null;
-		
-		int index = indexOf(element);
-		if (element != null && index != -1) {
-			result = (T) this.table[index];
-		}	
-		return result;
-	}
+   @Override
+   public void remove(T element) {
+      if (element != null && !this.isEmpty()) {
+         int probe = 0;
+         boolean removeu = false;
 
-	@Override
-	public int indexOf(T element) {
-		int result = -1;
-		
-		boolean achou = false;
-		int probe = 0;
-		if (element != null && !this.isEmpty()) {
-			while (probe < this.capacity() && !achou) {
-				int index = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, probe);
-				if (this.table[index] != null && this.table[index].equals(element)) {
-					result = index;
-					achou = true;
-				}
-				probe++;
-			}			
-		}	
-		return result;
-	}
+         while (probe < this.capacity() && !removeu) {
+            int index = ((HashFunctionQuadraticProbing<T>) this.hashFunction).hash(element, probe);
+            if (this.table[index] != null && this.table[index].equals(element)) {
+               this.table[index] = new DELETED();
+               this.elements--;
+               removeu = true;
+            }
+            probe++;
+         }
+      }
+   }
+
+   @Override
+   public T search(T element) {
+      T result = null;
+
+      int index = indexOf(element);
+      if (element != null && index != -1) {
+         result = element;
+      }
+      return result;
+   }
+
+   @Override
+   public int indexOf(T element) {
+      int result = -1;
+
+      boolean achou = false;
+      int probe = 0;
+      if (element != null && !this.isEmpty()) {
+         while (probe < this.capacity() && !achou) {
+            int index = ((HashFunctionQuadraticProbing<T>) this.hashFunction).hash(element, probe);
+            if (this.table[index] != null && this.table[index].equals(element)) {
+               result = index;
+               achou = true;
+            }
+            probe++;
+         }
+      }
+      return result;
+   }
 }
