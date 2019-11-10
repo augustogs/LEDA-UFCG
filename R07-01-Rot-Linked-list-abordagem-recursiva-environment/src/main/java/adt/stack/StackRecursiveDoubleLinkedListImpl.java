4f74f2cbeb6aac1e3b644cc8880de400c5e9674f
Augusto Santos <augusto.santos@ccc.ufcg.edu.br>
2019-11-10 15:44:08 -0300

    Ajuste repositório: Roteiro 07

--- R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/stack/StackRecursiveDoubleLinkedListImpl.java
+++ R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/stack/StackRecursiveDoubleLinkedListImpl.java
@@ -15,24 +15,15 @@ public class StackRecursiveDoubleLinkedListImpl<T> implements Stack<T> {
 
 	@Override
 	public void push(T element) throws StackOverflowException {
-<<<<<<< HEAD
-		// TODO Implement the method
-		throw new UnsupportedOperationException("Method not implemented");
-=======
 		if (isFull()) {
 			throw new StackOverflowException();
 		} else {
 			this.top.insert(element);
 		}
->>>>>>> f18c998a04623bfa6991a0bf88e16e456443ed55
 	}
 
 	@Override
 	public T pop() throws StackUnderflowException {
-<<<<<<< HEAD
-		// TODO Implement the method
-		throw new UnsupportedOperationException("Method not implemented");
-=======
 		if (isEmpty()) {
 			throw new StackUnderflowException();
 		} else {
@@ -40,41 +31,25 @@ public class StackRecursiveDoubleLinkedListImpl<T> implements Stack<T> {
 			this.top.removeFirst();
 			return result;
 		}
->>>>>>> f18c998a04623bfa6991a0bf88e16e456443ed55
 	}
 
 	@Override
 	public T top() {
-<<<<<<< HEAD
-		// TODO Implement the method
-		throw new UnsupportedOperationException("Method not implemented");
-=======
 		T result = null;
 		if (!isEmpty()) {
 			result = this.top.toArray()[top.size() - 1];
 		}
 		return result;
->>>>>>> f18c998a04623bfa6991a0bf88e16e456443ed55
 	}
 
 	@Override
 	public boolean isEmpty() {
-<<<<<<< HEAD
-		// TODO Implement the method
-		throw new UnsupportedOperationException("Method not implemented");
-=======
 		return this.size == this.top.size();
->>>>>>> f18c998a04623bfa6991a0bf88e16e456443ed55
 	}
 
 	@Override
 	public boolean isFull() {
-<<<<<<< HEAD
-		// TODO Implement the method
-		throw new UnsupportedOperationException("Method not implemented");
-=======
 		return this.size == this.top.size();
->>>>>>> f18c998a04623bfa6991a0bf88e16e456443ed55
 	}
 
 }
